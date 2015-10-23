1684bf3851d1082f2120456c8de6c41196d77110
Luo Xiaofei <xiaofeirocks@gmail.com>
2015-10-23 13:57:02 +0200

    no message

--- Ch2 Building Abstractions with Data/2.5 Systems with Generic Operations.rkt
+++ Ch2 Building Abstractions with Data/2.5 Systems with Generic Operations.rkt
@@ -307,10 +307,10 @@
 
 ; ex 2.84
 (define (apply-generic-raise op . args)
-
+  
   (define (error-msg)
     (error "No method for these types" (list op (map type-tag args))))
-
+  
   ; raise x to y
   (define (raise-args x y)
     (cond ((equal? x #f) #f)
@@ -341,17 +341,16 @@
               x))
         x)))
 
-
-; symbolic algebra
-
+; polynomial package
 (define (install-polynomial-package)
   ;; internal procedures
   ; poly
   (define (make-poly variable term-list)
     (cons variable term-list))
+  
   (define (variable poly) (car poly))
   (define (term-list poly) (cdr poly))
-
+  
   ; ex 2.87
   (define (zero-poly? p)
     (define (zero-terms? termlist)
@@ -360,20 +359,43 @@
           (and (=zero? (coeff (first-term termlist)))
                (zero-terms? (rest-terms termlist)))))
     (zero-terms? (term-list p)))
-    
+  
   ; terms
   (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
   (define (coeff term) (cadr term))
-  (define (the-empty-termlist) '())
-  (define (first-term term-list) (car term-list))
-  (define (rest-terms term-list) (cdr term-list))
-  (define (empty-termlist? term-list) (null? term-list))
+  
+  (define (the-empty-termlist term-list)
+    (let ((proc (get 'the-empty-termlist (type-tag term-list))))
+      (if proc
+          proc
+          (error "THE-EMPTY-TERMLIST" term-list))))
+  
+  ; generic ops
+  (define (first-term term-list)
+    (let ((proc (get 'first-term (type-tag term-list))))
+      (if proc
+          (proc term-list)
+          (error "FIRST-TERM" term-list))))
+  
+  (define (rest-terms term-list)
+    (let ((proc (get 'rest-terms (type-tag term-list))))
+      (if proc
+          (proc term-list)
+          (error "REST-TERMS" term-list))))
+  
+  (define (empty-termlist? term-list)
+    (let ((proc (get 'empty-termlist? (type-tag term-list))))
+      (if proc (proc term-list)
+          (error "EMPTY-TERMLIST?" term-list))))
+  
+  ; generic adjoin-term
   (define (adjoin-term term term-list)
-    (if (=zero? term)
-        term-list
-        (cons term term-list)))
-
+    (let ((proc (get 'adjoin-term (type-tag term-list))))
+      (if proc
+          (proc term term-list)
+          (error "No adjoin-term for this type" (list term term-list)))))
+  
   ; term operations
   (define (add-terms L1 L2)
     (cond ((empty-termlist? L1) L2)
@@ -390,26 +412,29 @@
                                 (add (coeff t1) (coeff t2)))
                      (add-terms (rest-terms L1)
                                 (rest-terms L2)))))))))
-
+  
   (define (neg-terms L)
-    (map (lambda (term) (make-term (order term) (- (coeff term))))
-         L))
+    (cons (type-tag L)
+          (map (lambda (term) (make-term (order term) (- (coeff term))))
+               (contents L))))
+  
+  (define (sub-terms L1 L2)
+    (add-terms L1 (neg-terms L2)))
   
   (define (mul-terms L1 L2)
     (if (empty-termlist? L1)
-        the-empty-termlist
+        (the-empty-termlist L1)
         (add-terms (mul-term-by-all-terms (first-term L1) L2)
                    (mul-terms (rest-terms L1) L2))))
-
+  
   (define (mul-term-by-all-terms t1 L)
     (if (empty-termlist? L)
-        the-empty-termlist
+        (the-empty-termlist L)
         (let ((t2 (first-term L)))
           (adjoin-term
            (make-term (+ (order t1) (order t2))
                       (mul (coeff t1) (coeff t2)))
            (mul-term-by-all-terms t1 (rest-terms L))))))
-
   
   ; poly operations
   (define variable? symbol?)
@@ -422,13 +447,17 @@
                    (add-terms (term-list p1)
                               (term-list p2)))
         (error "Polys not in same var -- ADD-POLY" (list p1 p2))))
-
+  
   (define (neg-poly p)
     (make-poly (variable p)
-               (neg-terms p)))
+               (neg-terms (term-list p))))
   
   (define (sub-poly p1 p2)
-    (add-poly p1 (neg-poly p2)))
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (sub-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polys not in same var -- SUB-POLY" (list p1 p2))))
   
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
@@ -436,7 +465,28 @@
                    (mul-terms (term-list p1)
                               (term-list p2)))
         (error "Polys not in same var -- MUL-POLY" (list p1 p2))))
-
+  
+  ; ex 2.91
+  (define (div-poly p1 p2)
+    (if (same-variable? (variable? p1) (variable p2))
+        (div-terms (term-list p1) (term-list p2))
+        (error "Not same variable" (list p1 p2))))
+  
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist L1) (the-empty-termlist L1))
+        (let ((t1 (first-term L1)) (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist L1) L1)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((new-t (cons (type-tag L1) (make-term new-o new-c))))
+                  (let ((rest-of-result (div-terms
+                                         (sub-terms L1 (mul-terms new-t L2))
+                                         L2)))
+                    (list (adjoin-term new-t (car rest-of-result))
+                          (cadr rest-of-result)))))))))
+  
   ; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial, polynomial)
@@ -453,4 +503,54 @@
   (put 'zero 'polynomial zero-poly?)
   'done)
 
-; ex 2.89, dense representation of term list
+; ex 2.89, ex 2.90 
+(define make-term list)
+(define order car)
+(define coeff cadr)
+
+(define (install-sparse-term-package)
+  (define (rest-terms term-list) (cons 'sparse (cddr term-list)))
+  (define the-empty-termlist '(sparse))
+  (define (empty-termlist? x) (equal? x the-empty-termlist))
+  (define (make-poly variable terms) (make-poly variable (cons 'sparse terms)))
+  
+  (define (adjoin-term term term-list)
+    (if (=zero? term)
+        term-list
+        (cons term term-list)))
+  
+  (define (first-term term-list) (car term-list))
+  
+  (define (tag x) (attach-tag 'sparse x))
+  (put 'adjoin-term 'sparse adjoin-term)
+  (put 'first-term 'sparse first-term)
+  (put 'rest-terms 'sparse rest-terms)
+  (put 'empty-termlist? 'sparse empty-termlist?)
+  (put 'the-empty-term-list 'sparse the-empty-termlist)
+  (put 'make 'sparse make-poly)
+  'done)
+
+(define (install-dense-term-package)
+  (define (rest-terms term-list) (cons 'dense (cddr term-list)))
+  (define the-empty-termlist '(dense))
+  (define (empty-termlist? x) (equal? x the-empty-termlist))
+  (define (make-poly variable terms) (make-poly variable (cons 'dense terms)))
+  
+  (define (first-term term-list)
+    (list (car term-list) (- (length term-list))))
+  
+  (define (adjoin-term term term-list)
+    (cond ((=zero? (coeff term)) term-list)
+          ((= (order term) (length term-list))
+           (cons (coeff term) term-list))
+          (else (adjoin-term
+                 term (cons 0 term-list)))))
+  
+  (define (tag x) (attach-tag 'dense x))
+  (put 'adjoin-term 'dense adjoin-term)
+  (put 'first-term 'dense first-term)
+  (put 'rest-terms 'dense rest-terms)
+  (put 'empty-termlist? 'dense empty-termlist?)
+  (put 'the-empty-term-list 'dense the-empty-termlist)
+  (put 'make 'dense make-poly)
+  'done)
