135f4219edad9713ef8a8b5328655f7e3a8ee10a
Luo Xiaofei <xiaofeirocks@gmail.com>
2015-10-27 19:53:16 +0100

    no message

--- CH3 Modularity, Objects, and State/3.3 Modeling with Mutable Data.rkt
+++ CH3 Modularity, Objects, and State/3.3 Modeling with Mutable Data.rkt
@@ -138,3 +138,238 @@
 (circular? (let ((x '(a))) (list x x))) ; #f
 (circular? (make-cycle (list 'a 'b 'c))) ; #t
 
+; ex 3.20, (cdr (cons a b)) => b; (car (cons a b)) => a
+(define (i-cons x y)
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+          ((eq? m 'cdr) y)
+          ((eq? m 'set-car!) (lambda (v) (set! x v)))
+          ((eq? m 'set-cdr!) (lambda (v) (set! y v)))
+          (else (error "Undefined operation -- CONS" m))))
+  dispatch)
+
+(define (i-car z) (z 'car))
+(define (i-cdr z) (z 'cdr))
+(define (set-i-car! z new-value) (z 'set-car! new-value))
+(define (set-i-cdr! z new-value) (z 'set-cdr! new-value))
+
+; queue
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           (print-queue queue))
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           (print-queue queue)))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         (print-queue queue))))
+
+; ex 3.21, pretty print queue
+(define (print-queue queue)
+  (display (front-ptr queue))
+  (newline))
+
+(define q (make-queue))
+(insert-queue! q 'a)
+(insert-queue! q 'b)
+(delete-queue! q)
+(delete-queue! q)
+
+; ex 3.22, build queue as a procedure with local state
+(define (make-queue-local)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (empty-queue?) (null? front-ptr))
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT! called with empty queue")
+          (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (list item)))
+        (cond ((empty-queue?)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair))
+              (else
+               (set-cdr! rear-ptr new-pair)
+               (set! rear-ptr new-pair)))
+        front-ptr))
+    (define (delete-queue!)
+      (cond ((empty-queue?)
+             (error "DELETE! called with empty queue"))
+            (else
+             (set! front-ptr (cdr front-ptr))
+             front-ptr)))
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue) empty-queue?)
+            ((eq? m 'front-queue) front-queue)
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) delete-queue!)
+            (else (error "Undefined operation -- QUEUE" m))))
+    dispatch))
+
+; ex 3.23, deque, using doubly linked list
+(define (make-deque) (cons nil nil))
+(define (empty-deque? deque) (null? (front-ptr deque)))
+
+(define (front-deque deque)
+  (if (empty-deque? deque)
+      (error "FRONT called with an empty deque" deque)
+      (caar (front-ptr deque))))
+  
+(define (rear-deque deque)
+  (if (empty-deque? deque)
+      (error "REAR called with an empty deque" deque)
+      (caar (rear-ptr deque))))
+  
+(define (front-insert-deque! deque item)
+  (let ((new-pair (cons (cons item nil) nil)))
+    (cond ((empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           (print-deque deque))
+          (else
+           (set-cdr! new-pair (front-ptr deque))
+           (set-cdr! (car (front-ptr deque)) new-pair)
+           (set-front-ptr! deque new-pair)
+           (print-deque deque)))))
+           
+(define (rear-insert-deque! deque item)
+  (let ((new-pair (cons (cons item nil) nil)))
+    (cond ((empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           (print-deque deque))
+          (else
+           (set-cdr! (rear-ptr deque) new-pair)
+           (set-cdr! (car new-pair) (rear-ptr deque))
+           (set-rear-ptr! deque new-pair)
+           (print-deque deque)))))
+
+(define (front-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (error "DELETE! called with an empty deque" deque))
+        (else
+         (set-front-ptr! deque (cdr (front-ptr deque)))
+         (print-deque deque))))
+
+(define (rear-delete-deque! deque)
+  (cond ((empty-deque? deque)
+         (error ("DELETE! called with an empty deque" deque)))
+        (else
+         (set-rear-ptr! deque (cdar (rear-ptr deque)))
+         (set-cdr! (rear-ptr deque) nil)
+         (print-deque deque))))
+
+(define (print-deque deque)
+  (define (iter q)
+    (if (or (null? q) (empty-deque? q))
+        (display "")
+        (begin (display (caar q)) (iter (cdr q)))))
+  (iter (front-ptr deque)) (newline))
+
+(define dq (make-deque))
+(front-insert-deque! dq 'b) ; b
+(front-insert-deque! dq 'a) ; ab
+(rear-insert-deque! dq 'c) ; abc
+(rear-delete-deque! dq) ; ab
+(front-delete-deque! dq) ; b
+(front-delete-deque! dq) ; '()
+
+; 1D table
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        #f)))
+
+(define (assoc key records)
+  (cond ((null? records) #f)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table))))))
+
+(define (make-table) (list '*table*))
+
+; 2D table
+(define (lookup key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (cdr record)
+              #f))
+        #f)))
+
+(define (insert! key-1 key-2 value table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key-1
+                              (cons key-2 value))
+                        (cdr table))))))
+
+(define (make-table-object)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table))))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
