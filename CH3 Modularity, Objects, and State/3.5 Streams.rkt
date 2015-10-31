9d7a60b8f1215725af9c8e95d85c27bd3cb33510
Luo Xiaofei <xiaofeirocks@gmail.com>
2015-10-31 13:52:02 +0100

    no message

--- CH3 Modularity, Objects, and State/3.5 Streams.rkt
+++ CH3 Modularity, Objects, and State/3.5 Streams.rkt
@@ -154,6 +154,10 @@
 (define (add-streams s1 s2) (stream-map-general + s1 s2))
 (define integers-2 (cons-stream 1 (add-streams ones integers)))
 
+; ex 3.57
+; nth fib is the sum of two previous fibs. if fibs are memorized,
+; f(n) = f(n-1) + f(n-2), one additional add is needed, and fib is
+; linear; if not, we have to recompute f(n-1) and f(n-2), fib is exponential.
 (define fibs-2
   (cons-stream
    0
@@ -183,7 +187,7 @@
 ; ex 3.53
 (define s (cons-stream 1 (add-streams s s))) ; stream of 2n
 
-; ex 3.54
+; ex 3.54, multiply streams
 (define (mul-streams s1 s2) (stream-map-general * s1 s2))
 
 (define factorials
@@ -191,7 +195,7 @@
 
 (stream-ref factorials 4)
 
-; ex 3.55
+; ex 3.55, S0, S0 + S1, S0 + S1 + S2
 (define (partial-sums S)
   (cons-stream (stream-car S)
                (add-streams (stream-cdr S)
@@ -199,7 +203,7 @@
 
 (stream-ref (partial-sums integers) 4) ; 15
 
-; ex 3.56
+; ex 3.56, merge streams
 (define (merge s1 s2)
   (cond ((stream-null? s1) s2)
         ((stream-null? s2) s1)
@@ -226,10 +230,158 @@
                                   (scale-stream S 5)))))
 
 ; print the first n elemnts of a stream
-(define (print-stream-n s n)
+(define (display-first-terms stream n)
   (if (> n 0)
-      (begin (display (stream-car s))
+      (begin (display (stream-car stream))
              (display " ")
-             (print-stream-n (stream-cdr s) (dec n)))))
+             (display-first-terms (stream-cdr stream) (dec n)))))
 
-(print-stream-n SS 10) (newline)
+(display-first-terms SS 10) (newline)
+
+; ex 3.58, [num/den] in radix
+(define (expand num den radix)
+  (cons-stream
+   (quotient (* num radix) den)
+   (expand (remainder (* num radix) den) den radix)))
+
+(display-first-terms (expand 1 7 10) 10) (newline)
+(display-first-terms (expand 3 8 10) 10) (newline)
+
+
+; ex 3.59, power series
+(define (integrate-series coeff-stream)
+  (stream-map-general * (stream-map-general / ones integers) coeff-stream))
+
+(define exp-series (cons-stream 1 (integrate-series exp-series)))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
+(define cosine-series (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+
+(display-first-terms exp-series 5) (newline) ; 1 1 1/2 1/6 1/24
+(display-first-terms (integrate-series integers) 5) (newline) ; 1 1 1 1 1
+(display-first-terms cosine-series 5) (newline) ; 1 0 -1/2 0 1/24 
+(display-first-terms sine-series 5) (newline) ; 0 1 0 -1/6 0
+
+; ex 3.60, multiply series
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams
+                (scale-stream (stream-cdr s2) (stream-car s1))
+                (mul-series (stream-cdr s1) s2))))
+
+(define sinx^2+cosx^2
+  (add-streams
+   (mul-series sine-series sine-series)
+   (mul-series cosine-series cosine-series)))
+
+(display-first-terms sinx^2+cosx^2 5) (newline) ; 1 0 0 0 0
+
+; ex 3.61, invert series
+(define (invert-series s)
+  (cons-stream 1
+               (mul-series (scale-stream (stream-cdr s) -1)
+                           (invert-series s))))
+
+(define unit
+  (mul-series cosine-series
+              (invert-series cosine-series)))
+
+(display-first-terms unit 5) (newline) ; 1 0 0 0 0
+
+; ex 3.62, devide series
+(define (div-series s1 s2)
+  (let ((constant-denom (stream-car s2)))
+    (if (= constant-denom 0)
+        (error "denominator has zero constant term -- DIV-SERIES" s2)
+        (mul-series s1
+                    (scale-stream
+                     (invert-series
+                      (scale-stream s2
+                                    (/ 1 constant-denom)))
+                     (/ 1 constant-denom))))))
+
+(define tangent-series
+  (div-series sine-series cosine-series))
+
+(display-first-terms tangent-series 7) (newline) ; 0 1 0 1/3 0 2/15 0
+
+
+; Stream Paradigm: represent state as a timeless stream of values
+; rather than a set of variables to be updated.
+
+; implement sqrt using streams
+(define (sqrt-improve guess x)
+  (define (average a b) (/ (+ a b) 2.0))
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream
+     1.0
+     (stream-map (lambda (guess) (sqrt-improve guess x))
+                 guesses)))
+  guesses)
+
+(display-first-terms (sqrt-stream 2) 7) (newline)
+
+; estimate pi using streams
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (scale-stream (pi-summands (+ n 2)) -1)))
+
+(define pi-stream (scale-stream (partial-sums (pi-summands 1)) 4))
+(display-first-terms pi-stream 7) (newline)
+
+; sequence accelerator
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0)) ; S(n-1)
+        (s1 (stream-ref s 1)) ; S(n)
+        (s2 (stream-ref s 2))) ; S(n+1)
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(display-first-terms (euler-transform pi-stream) 7) (newline)
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
+
+(display-first-terms
+ (accelerated-sequence euler-transform pi-stream) 7) (newline)
+
+; ex 3.63
+; without the local variable, the results won't be memorized, to compute
+; the n-th term, we recompute the (n-1)th term, (n-2)th term, and so on.
+(define (sqrt-stream-slow x)
+  (cons-stream 1.0
+               (stream-map (lambda (guess)
+                             (sqrt-improve guess x))
+                           (sqrt-stream-slow x))))
+
+; ex 3.64
+(define (stream-limit stream tolerance)
+  (let ((first (stream-car stream))
+        (second (stream-car (stream-cdr stream))))
+    (if (> tolerance (abs (- first second)))
+        second
+        (stream-limit (stream-cdr stream) tolerance))))
+
+(define (sqrt x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
+
+(sqrt 2.0 0.01)
+
+; ex 3.65
+(define (ln2-summands n)
+  (cons-stream (/ 1.0 n)
+               (scale-stream (ln2-summands (inc n)) -1)))
+
+(define ln2-stream (partial-sums (ln2-summands 1)))
+(define accelerated-ln2 (accelerated-sequence euler-transform ln2-stream))
+
+(display-first-terms accelerated-ln2 7) (newline)
