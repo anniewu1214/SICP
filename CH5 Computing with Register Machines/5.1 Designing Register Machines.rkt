d145918ecf4d9e6e5873a4512247cf5135322675
Luo Xiaofei <xiaofeirocks@gmail.com>
2015-12-08 15:10:36 +0100

    review

--- CH5 Computing with Register Machines/5.1 Designing Register Machines.rkt
+++ CH5 Computing with Register Machines/5.1 Designing Register Machines.rkt
@@ -4,13 +4,15 @@
 (define gcd-machine
   (make-machine
    (list (list 'rem remainder) (list '= =))
-   '(test-b
+   '(
+     test-b
      (test (op =) (reg b) (const 0))
      (branch (label gcd-done))
      (assign t (op rem) (reg a) (reg b))
      (assign a (reg b))
      (assign b (reg t))
      (goto (label test-b))
+     
      gcd-done)))
 
 
@@ -26,14 +28,17 @@
 (define fac-machine
   (make-machine
    (list (list '* *) (list '+ +) (list '> >))
-   '((assign product (const 1))
+   '(
+     (assign product (const 1))
      (assign counter (const 1))
+     
      test-counter
      (test (op >) (reg counter) (reg n))
      (branch (label fac-done))
      (assign product (op *) (reg product) (reg counter))
      (assign counter (op +) (reg counter) (const 1))
      (goto (label test-counter))
+     
      fac-done)))
 
 
@@ -47,9 +52,11 @@
 (define gcd-machine-read-inputs
   (make-machine
    (list (list 'read read) (list '= =) (list 'rem remainder) (list 'print display))
-   '(gcd-loop
+   '(
+     gcd-loop
      (assign a (op read))
      (assign b (op read))
+     
      test-b   
      (test (op =) (reg b) (const 0))
      (branch (label gcd-done))
@@ -57,6 +64,7 @@
      (assign a (reg b))
      (assign b (reg t))
      (goto (label test-b))
+     
      gcd-done
      (perform (op print) (reg a))
      (goto (label gcd-loop)))))
@@ -70,19 +78,23 @@
 (define elaborated-gcd-machine
   (make-machine
    (list (list 'rem remainder) (list '= =) (list '< <) (list '- -))
-   '(test-b
+   '(
+     test-b
      (test (op =) (reg b) (const 0))
      (branch (label gcd-done))
      (assign t (reg a))
+     
      rem-loop
      (test (op <) (reg t) (reg b))
      (branch (label rem-done))
      (assign t (op -) (reg t) (reg b))
      (goto (label rem-loop))
+     
      rem-done
      (assign a (reg b))
      (assign b (reg t))
      (goto (label test-b))
+     
      gcd-done)))
 
 
@@ -105,13 +117,16 @@
 (define sqrt-machine
   (make-machine
    (list (list 'good-enough? good-enough?) (list 'read read) (list 'improve improve))
-   '((assign x (op read))
+   '(
+     (assign x (op read))
      (assign guess (const 1.0))
+     
      test-good
      (test (op good-enough?) (reg guess) (reg x))
      (branch (label sqrt-done))
      (assign guess (op improve) (reg guess) (reg x))
      (goto (label test-good))
+     
      sqrt-done)))
 
 
@@ -125,15 +140,18 @@
   (make-machine
    (list (list 'read read) (list '* *) (list '> >) (list '< <)
          (list '/ /) (list '+ +) (list '- -) (list 'print display))
-   '(sqrt-loop
+   '(
+     sqrt-loop
      (assign x (op read))
      (assign guess (const 1.0))
+     
      test-good
      (assign temp (op *) (reg guess) (reg guess))
      (assign temp (op -) (reg temp) (reg x))
      (test (op >) (reg temp) (const 0))
      (branch (label abs-done))
      (assign temp (op -) (const 0) (reg temp))
+     
      abs-done
      (test (op <) (reg temp) (const 0.001))
      (branch (label sqrt-done))
@@ -141,6 +159,7 @@
      (assign temp (op +) (reg temp) (reg guess))
      (assign guess (op /) (reg temp) (const 2))
      (goto (label test-good))
+     
      sqrt-done
      (perform (op print) (reg guess))
      (goto (label sqrt-loop)))))
@@ -155,7 +174,9 @@
 (define recursive-fact-machine
   (make-machine
    (list (list '= =) (list '- -) (list '* *))
-   '((assign continue (label fact-done))   ; set up final return address
+   '(
+     (assign continue (label fact-done))   ; set up final return address
+     
      fact-loop
      (test (op =) (reg n) (const 1))
      (branch (label base-case))
@@ -167,14 +188,17 @@
      (assign n (op -) (reg n) (const 1))
      (assign continue (label after-fact))
      (goto (label fact-loop))
+     
      after-fact
      (restore n)
      (restore continue)
      (assign val (op *) (reg n) (reg val)) ; val now contains n(n-1)!
      (goto (reg continue))                 ; return to caller
+     
      base-case
      (assign val (const 1))                ; base case: 1! = 1
      (goto (reg continue))                 ; return to caller
+     
      fact-done)))
 
 
@@ -189,7 +213,9 @@
 (define recursive-exp-machine
   (make-machine
    (list (list '= =) (list '- -) (list '* *))
-   '((assign continue (label exp-done))
+   '(
+     (assign continue (label exp-done))
+     
      exp-loop
      (test (op =) (reg n) (const 0))
      (branch (label base-case))
@@ -197,13 +223,16 @@
      (assign n (op -) (reg n) (const 1))
      (assign continue (label after-exp))
      (goto (label exp-loop))
+     
      after-exp
      (restore continue)
      (assign val (op *) (reg b) (reg val))
      (goto (reg continue))
+     
      base-case
      (assign val (const 1))
      (goto (reg continue))
+     
      exp-done)))
 
 
@@ -218,13 +247,16 @@
 (define iterative-exp-machine
   (make-machine
    (list (list '= =) (list '- -) (list '* *))
-   '((assign product (const 1))
+   '(
+     (assign product (const 1))
+     
      exp-loop
      (test (op =) (reg n) (const 0))
      (branch (label exp-done))
      (assign product (op *) (reg b) (reg product))
      (assign n (op -) (reg n) (const 1))
      (goto (label exp-loop))
+     
      exp-done)))
 
 
@@ -242,6 +274,7 @@
    (list (list '< <) (list '- -) (list '+ +))
    '(
      (assign continue (label fib-done))
+     
      fib-loop
      (test (op <) (reg n) (const 2))
      (branch (label immediate-answer))
@@ -250,6 +283,7 @@
      (save n)
      (assign n (op -) (reg n) (const 1))
      (goto (label fib-loop))
+     
      afterfib-n-1
      (restore n)
      ;(restore continue)
@@ -258,6 +292,7 @@
      (assign continue (label afterfib-n-2))
      (save val)
      (goto (label fib-loop))
+     
      afterfib-n-2
      ;(assign n (reg val))
      ;(restore val)
@@ -266,9 +301,11 @@
      (restore continue)
      (assign val (op +) (reg val) (reg n)) 
      (goto (reg continue))
+     
      immediate-answer
      (assign val (reg n))
      (goto (reg continue))
+     
      fib-done)))
 
 
--- CH5 Computing with Register Machines/5.3 Storage Allocation and Garbage Collection.rkt
+++ CH5 Computing with Register Machines/5.3 Storage Allocation and Garbage Collection.rkt
@@ -1,67 +1,100 @@
 #lang planet neil/sicp
 
 ; primitive memory operations
-(define (vector-ref <vector> <n>)
-  "return the nth element of the vector")
 
-(define (vector-set! <vector> <n> <value>)
-  "set the nth element of the vector to <value>")
+(define (vector-ref <vector> <n>) "return the nth element of <vector>")
+(define (vector-set! <vector> <n> <value>) "set the nth element of <vector> to <value>")
+
 
 
 ; implementing primitive list operations
-; * assume that operations on pointers use only the index portion of the typed pointer
+;; assume that operations on pointers use only the index portion of the typed pointer
+
 
 ;; car, cdr
+
 (assign <reg1> (op car) (reg <reg2>))
-(assign <reg1> (op cdr) (reg <reg2>)) ; =>
+(assign <reg1> (op cdr) (reg <reg2>))
+; =>
 (assign <reg1> (op vector-ref) (reg the-cars) (reg <reg2>))
 (assign <reg1> (op vector-ref) (reg the-cdrs) (reg <reg2>))
 
+
 ;; set-car!, set-cdr!
+
 (perform (op set-car!) (reg <reg1>) (reg <reg2>))
-(perform (op set-cdr!) (reg <reg1>) (reg <reg2>)) ; =>
+(perform (op set-cdr!) (reg <reg1>) (reg <reg2>))
+; =>
 (perform (op vector-set!) (reg the-cars) (reg <reg1>) (reg <reg2>))
 (perform (op vector-set!) (reg the-cdrs) (reg <reg1>) (reg <reg2>))
 
+
 ;; cons
-(assign <reg1> (op cons) (reg <reg2>) (reg <reg3>)) ; =>
+
+(assign <reg1> (op cons) (reg <reg2>) (reg <reg3>))
+; =>
 (perform (op vector-set!) (reg the-cars) (reg free) (reg <reg2>))
 (perform (op vector-set!) (reg the-cdrs) (reg free) (reg <reg3>))
 (assign <reg1> (reg free))
 (assign free (op +) (reg free) (const 1))
 
-;; eq?: tests the equality of all fields in the registers
-;;      equality of data objects <=> identicality of their pointers
-;; pair?, null?, symbol?, number? : check the type field
+
+;; eq?
+;  tests the equality of all fields in the registers
+;  equality of data objects <=> identicality of their pointers
+
+;; pair?, null?, symbol?, number?
+;  check the type field
+
+
 
 
 ; implementing stacks
-(save <reg>) ; =>
+
+
+;; save
+(save <reg>)
+; =>
 (assign the-stack (op cons) (reg <reg>) (reg the-stack))
 
-(restore <reg>) ; =>
+
+;; resotre
+(restore <reg>)
+; =>
 (assign <reg> (op car) (reg the-stack))
 (assign the-stack (op cdr) (reg the-stack))
 
-(perform (op initialize-stack)) ; =>
+
+;; perform
+(perform (op initialize-stack))
+; =>
 (assign the-stack (const ()))
 
+
+
 ; ex 5.20
+;
 ; 0   1   2   3   4
 ;     n1  p1  p1 
 ;     n2  p3  e0
+;
 ; the final value of free is p4
 ; x => index 1   y => index 2
 
 
+
+
 ; ex 5.21
+
 ;; recursive procedure
+
 (define recursive-count-leaves-machine
   (make-machine
    (list (list '+ +) (list 'null? null?) (list 'pair? pair?)
          (list 'car car) (list 'cdr cdr))
-   '((assign continue (label count-leaves-done))
-     (assign val (const 0))
+   '(
+     (assign continue (label count-leaves-done))
+     
      count-loop
      (test (op null?) (reg tree))
      (branch (label null-tree))
@@ -69,12 +102,14 @@
      (branch (label left-branch))
      (assign val (const 1))
      (goto (reg continue))
+     
      left-branch
      (save tree)
      (save continue)
      (assign continue (label right-branch))
      (assign tree (op car) (reg tree))
      (goto (label count-loop))
+     
      right-branch
      (restore continue)
      (restore tree)
@@ -83,28 +118,37 @@
      (assign continue (label after-count))
      (assign tree (op cdr) (reg tree))
      (goto (label count-loop))
+     
      after-count
      (assign var (reg val))
      (restore val)
      (restore continue)
      (assign val (op +) (reg var) (reg val))
      (goto (reg continue))
+     
      null-tree
      (assign val (const 0))
      (goto (reg continue))
+     
      count-leaves-done)))
+
 
 (set-register-contents! recursive-count-leaves-machine 'tree '(a (b c (d)) (e f) g))
 (start recursive-count-leaves-machine)
 (get-register-contents recursive-count-leaves-machine 'val) ; 7
 
+
+
 ;; iterative procedure
+
 (define iterative-count-leaves-machine 
   (make-machine 
    (list (list '+ +) (list 'null? null?) (list 'pair? pair?)
          (list 'car car) (list 'cdr cdr))
-   '((assign n (const 0))
+   '(
+     (assign n (const 0))
      (assign continue (label count-leaves-done))
+     
      count-loop
      (test (op null?) (reg tree))
      (branch (label null-tree))
@@ -112,31 +156,41 @@
      (branch (label pair-tree))
      (assign n (op +) (reg n) (const 1))
      (goto (reg continue))
-     null-tree
-     (goto (reg continue))
+     
      pair-tree
      (save continue)
      (save tree)
      (assign tree (op car) (reg tree))
      (assign continue (label after-left-branch))
      (goto (label count-loop))
+     
      after-left-branch
      (restore tree)
      (assign tree (op cdr) (reg tree))
      (assign continue (label after-right-branch))
      (goto (label count-loop))
+     
      after-right-branch
      (restore continue)
      (goto (reg continue))
+     
+     null-tree
+     (assign val (reg n));
+     (goto (reg continue))
+     
      count-leaves-done)))
+
 
 (set-register-contents! iterative-count-leaves-machine 'tree '(a (b c (d)) (e f) g))
 (start iterative-count-leaves-machine)
 (get-register-contents iterative-count-leaves-machine 'n) ; 7
 
 
+
 ; ex 5.22
+
 ;; append
+
 (define (append x y)
   (if (null? x)
       y
@@ -146,7 +200,9 @@
   (make-machine
    (list (list 'car car) (list 'cdr cdr)
          (list 'cons cons) (list 'null? null?))
-   '((assign continue (label append-done))
+   '(
+     (assign continue (label append-done))
+     
      append-loop
      (test (op null?) (reg x))
      (branch (label null-x))
@@ -156,22 +212,29 @@
      (assign x (op cdr) (reg x))
      (assign continue (label after-append))
      (goto (label append-loop))
+     
      after-append
      (restore car-x)
      (restore continue)
      (assign x (op cons) (reg car-x) (reg x))
      (goto (reg continue))
+     
      null-x
      (assign x (reg y))
      (goto (reg continue))
+     
      append-done)))
+
 
 (set-register-contents! append-machine 'x '(1 2))
 (set-register-contents! append-machine 'y '(3 4))
 (start append-machine)
 (get-register-contents append-machine 'x) ; (1 2 3 4)
 
+
+
 ;; append!
+
 (define (last-pair x)
   (if (null? (cdr x))
       x
@@ -181,19 +244,26 @@
   (set-cdr! (last-pair x) y)
   x)
 
+
 (define append!-machine
   (make-machine
-   (list (list 'null? null?) (list 'cdr cdr) (list 'set-cdr! set-cdr!))
-   '((assign temp (reg x))
-      test-null
-      (assign cdr-x (op cdr) (reg temp))
-      (test (op null?) (reg cdr-x))
-      (branch (label null-cdr-x))
-      (assign temp (op cdr) (reg temp))
-      (goto (label test-null))
-      null-cdr-x
-      (perform (op set-cdr!) (reg temp) (reg y))
-      append!-done)))
+   (list (list 'null? null?) (list 'cdr cdr)
+         (list 'set-cdr! set-cdr!))
+   '(
+     (assign val (reg x))
+     
+     last-pair-loop
+     (assign cdr-x (op cdr) (reg val))
+     (test (op null?) (reg cdr-x))
+     (branch (label null-cdr-x))
+     (assign val (op cdr) (reg val))
+     (goto (label last-pair-loop))
+     
+     null-cdr-x
+     (perform (op set-cdr!) (reg val) (reg y))
+     
+     append!-done)))
+
 
 (set-register-contents! append!-machine 'x '(1 2))
 (set-register-contents! append!-machine 'y '(3 4))
@@ -201,57 +271,69 @@
 (get-register-contents append!-machine 'x) ; (1 2 3 4)
 
 
+
+
 ; implementation of a stop-and-copy garbage collector
-(define gc-instructions
-  (
-  begin-garbage-collection
+
+(define garbage-collection-instructions
+  '(
+   begin-garbage-collection
     (assign free (const 0))
     (assign scan (const 0))
     (assign old (reg root)) ; ptr to obj to be relocated
     (assign relocate-continue (label reassign-root))
     (goto (label relocate-old-result-in-new))
-  reassign-root
+    
+    reassign-root
     (assign root (reg new)) ; ptr to the relocated obj
     (goto (label gc-loop))
-  gc-loop
+    
+    gc-loop
     (test (op =) (reg scan) (reg free))
     (branch (label gc-flip))
     (assign old (op vector-ref) (reg new-cars) (reg scan))
     (assign relocate-continue (label update-car))
     (goto (label relocate-old-result-in-new))
-  update-car
+    
+    update-car
     (perform (op vector-set!) (reg new-cars) (reg scan) (reg new))
     (assign old (op vector-ref) (reg new-cdrs) (reg scan))
     (assign relocate-continue (label update-cdr))
     (goto (label relocate-old-result-in-new))
-  update-cdr
+    
+    update-cdr
     (perform (op vector-set!) (reg new-cdrs) (reg scan) (reg new))
     (assign scan (op +) (reg scan) (const 1))
     (goto (label gc-loop))
-  relocate-old-result-in-new
+    
+    relocate-old-result-in-new
     (test (op pointer-to-pair?) (reg old))
     (branch (label pair))
     (assign new (reg old))
     (goto (reg relocate-continue))
-  pair
+    
+    pair
     (assign oldcr (op vector-ref) (reg the-cars) (reg old))
     (test (op broken-heart?) (reg oldcr))
     (branch (label already-moved))
-    (assign new (reg free))
-    (assign free (op +) (reg free) (const 1))
+    (assign new (reg free))                    ; new location for pair
+    (assign free (op +) (reg free) (const 1))  ; update free pointer
+    ;; copy the car and cdr to new memory
     (perform (op vector-set!) (reg new-cars) (reg new) (reg oldcr))
     (assign oldcr (op vector-set!) (reg new-cdrs) (reg new) (reg oldcr))
+    ;; construct the broken heart
     (perform (op vector-set!) (reg the-cars) (reg old) (const broken-heart))
     (perform (op vector-set!) (reg the-cdrs) (reg old) (reg new))
     (goto (reg relocate-continue))
-  already-moved
+    
+    already-moved
     (assign new (op vector-ref) (reg the-cdrs) (reg old))
     (goto (reg relocate-continue))
-  gc-flip
+    
+    gc-flip
     (assign temp (reg the-cdrs))
     (assign the-cdrs (reg new-cdrs))
     (assign new-cdrs (reg temp))
     (assign temp (reg the-cars))
     (assign the-cars (reg new-cars))
-    (assign new-cars (reg temp))
-  'gc-done))
+    (assign new-cars (reg temp))))
